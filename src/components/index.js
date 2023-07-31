36232dbf6e4df48137c1a3f9af00be6909d3548b
Michael N Mwanza <mwanza.n.m@gmail.com>
2023-07-31 23:45:02 +0200

    Initial commit

---
+++ src/components/index.js
@@ -0,0 +1,30 @@
+import EarthCanvas from "./canvas/Earth";
+import BallCanvas from "./canvas/Ball";
+import ComputersCanvas from "./canvas/Computers";
+import StarsCanvas from "./canvas/Stars"
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import About from "./About";
+import Tech from "./Tech";
+import Experience from "./Experience";
+import Works from "./Works";
+import Feedbacks from "./Feedbacks";
+import Contact from "./Contact";
+import CanvasLoader from "./Loader";
+
+
+export {
+  Hero,
+  Navbar,
+  About,
+  Tech,
+  Experience,
+  Works,
+  Feedbacks,
+  Contact,
+  CanvasLoader,
+  EarthCanvas, 
+  BallCanvas, 
+  ComputersCanvas, 
+  StarsCanvas
+};
